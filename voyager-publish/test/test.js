1aba553577cdd94e57062451ce64c454b010916a
Julian Gruber <julian@juliangruber.com>
2024-03-05 14:49:34 +0100

    publish: fix test

--- voyager-publish/test/test.js
+++ voyager-publish/test/test.js
@@ -262,7 +262,7 @@ const insertMeasurement = async (client, measurement) => {
     completed_at_round
   )
   VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17
   )
 `, [
     measurement.zinniaVersion,
