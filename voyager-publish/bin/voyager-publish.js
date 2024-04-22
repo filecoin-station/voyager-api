6ea0bba70b4112012595e1a53f14a981b8b76b9a
Julian Gruber <julian@juliangruber.com>
2024-04-22 16:17:52 +0200

    fix lint

--- voyager-publish/bin/voyager-publish.js
+++ voyager-publish/bin/voyager-publish.js
@@ -44,12 +44,7 @@ console.log(
 let rpcUrlIndex = 0
 
 const client = new pg.Pool({ connectionString: DATABASE_URL })
-
-await client.query(
-  `UPDATE measurements SET lock = NULL WHERE lock IS NOT NULL`,
-  [ps.pid]
-)
-
+await client.query('UPDATE measurements SET lock = NULL WHERE lock IS NOT NULL')
 await Promise.all(new Array(CONCURRENCY).fill().map(() => async () => {
   while (true) {
     const lastStart = new Date()
@@ -80,7 +75,7 @@ await Promise.all(new Array(CONCURRENCY).fill().map(() => async () => {
       rpcUrlIndex++
     }
     await client.query(
-      `UPDATE measurements SET lock = NULL WHERE lock = $1`,
+      'UPDATE measurements SET lock = NULL WHERE lock = $1',
       [ps.pid]
     )
     const dt = new Date() - lastStart
