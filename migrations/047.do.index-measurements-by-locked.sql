e7f83479da3d4115f0bace3cbc42e762e2b6c331
Julian Gruber <julian@juliangruber.com>
2024-05-06 17:28:19 +0200

    fix migration

--- migrations/047.do.index-measurements-by-locked.sql
+++ migrations/047.do.index-measurements-by-locked.sql
@@ -1,3 +1,3 @@
 CREATE INDEX CONCURRENTLY measurements_locked
-  ON measurements (locked_by_id)
-  WHERE locked_by_id IS NOT NULL;
+  ON measurements (locked_by_pid)
+  WHERE locked_by_pid IS NOT NULL;
